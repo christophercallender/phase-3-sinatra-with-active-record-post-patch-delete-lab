d818d69c4dc68404d6e26f009e4f63c6c543045e
Christopher Callender <christophercallender0000@gmail.com>
2022-08-06 02:33:13 -0400

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,15 +12,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/baked_goods/by_price' do
-    # see the BakedGood class for the  method definition of `.by_price`
     baked_goods = BakedGood.by_price
     baked_goods.to_json
   end
 
   get '/baked_goods/most_expensive' do
-    # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    baked_good = BakedGood.create(params)
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(params)
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+  end
 end
